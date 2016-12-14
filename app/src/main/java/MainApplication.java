9c3856522a953a7a2341b503dc2fba270f2975e0
ljw <ljw@ljwdeiMac.local>
2016-12-14 10:44:08 +0800

    First commit

---
+++ app/src/main/java/MainApplication.java
@@ -0,0 +1,9 @@
+import android.app.Application;
+
+/**
+ * Created by ljw on 16/12/14.
+ */
+
+public class MainApplication extends Application {
+
+}
---
+++ app/src/main/java/com/example/ljw/myapplicationframe/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.ljw.myapplicationframe;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import de.greenrobot.dao.query.Query;
+import de.greenrobot.dao.query.QueryBuilder;
+import ljw.myapplicationframe.greendao.DaoMaster;
+import ljw.myapplicationframe.greendao.DaoSession;
+import ljw.myapplicationframe.greendao.Note;
+import ljw.myapplicationframe.greendao.NoteDao;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabase db;
+    private EditText editText;
+    private DaoMaster daoMaster;
+    private DaoSession daoSession;
+    private Cursor cursor;
+    public static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    private void setupDatabase() {
+        // 通过 DaoMaster 的内部类 DevOpenHelper，你可以得到一个便利的 SQLiteOpenHelper 对象。
+        // 可能你已经注意到了，你并不需要去编写「CREATE TABLE」这样的 SQL 语句，因为 greenDAO 已经帮你做了。
+        // 注意：默认的 DaoMaster.DevOpenHelper 会在数据库升级时，删除所有的表，意味着这将导致数据的丢失。
+        // 所以，在正式的项目中，你还应该做一层封装，来实现数据库的安全升级。
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "notes-db", null);
+        db = helper.getWritableDatabase();
+        // 注意：该数据库连接属于 DaoMaster，所以多个 Session 指的是相同的数据库连接。
+        daoMaster = new DaoMaster(db);
+        daoSession = daoMaster.newSession();
+    }
+
+    private NoteDao getNoteDao() {
+        return daoSession.getNoteDao();
+    }
+
+    private void search() {
+        // Query 类代表了一个可以被重复执行的查询
+        Query query = getNoteDao().queryBuilder()
+                .where(NoteDao.Properties.Text.eq("Test1"))
+                .orderAsc(NoteDao.Properties.Date)
+                .build();
+
+//      查询结果以 List 返回
+//      List notes = query.list();
+        // 在 QueryBuilder 类中内置两个 Flag 用于方便输出执行的 SQL 语句与传递参数的值
+        QueryBuilder.LOG_SQL = true;
+        QueryBuilder.LOG_VALUES = true;
+    }
+    private void addNote() {
+        String noteText = editText.getText().toString();
+        editText.setText("");
+
+        final DateFormat df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
+        String comment = "Added on " + df.format(new Date());
+
+        // 插入操作，简单到只要你创建一个 Java 对象
+        Note note = new Note(null, noteText, comment, new Date());
+        getNoteDao().insert(note);
+        Log.d(TAG, "Inserted new note, ID: " + note.getId());
+        cursor.requery();
+    }
+}
